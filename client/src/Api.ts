18ea3f0300900301c5eb8b3ce4b485993940ebb5
lucas-burdell <lucasburdell@gmail.com>
2019-06-22 00:20:18 -0500

    fix socketio goof; I am stupid

--- client/src/Api.ts
+++ client/src/Api.ts
@@ -1,20 +1,23 @@
-import openSocket from "socket.io-client";
-import { useEffect } from "react";
-const socket = openSocket(`http://${window.location.hostname}:8080`);
+import openSocket from 'socket.io-client';
+import { useEffect } from 'react';
+const socket = openSocket();
 
 export interface IPart {
   name: string;
 }
 
-export function useSubcribeToNewPart(callback: (part: IPart) => void, ...args: any[]) {
-    useEffect(() => {
-        socket.on("newpart", callback);
-        return () => {socket.off("newpart", callback)};
-    }, [callback, ...args]);
+export function useSubcribeToNewPart(
+  callback: (part: IPart) => void,
+  ...args: any[]
+) {
+  useEffect(() => {
+    socket.on('newpart', callback);
+    return () => {
+      socket.off('newpart', callback);
+    };
+  }, [callback, ...args]);
 }
-
 
 export function createPart() {
-  socket.emit("createpart");
+  socket.emit('createpart');
 }
-
